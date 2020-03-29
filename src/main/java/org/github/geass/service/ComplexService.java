6e49e8e8df34ef16259e7ff9d351af0de9ff52e5
Bo Zhang <zhangbodut@gmail.com>
2020-03-29 11:59:44 +0800

    Some idea

--- src/main/java/org/github/geass/service/ComplexService.java
+++ src/main/java/org/github/geass/service/ComplexService.java
@@ -5,6 +5,9 @@ import org.github.geass.dto.ServiceTypeB;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
 public class ComplexService {
 
@@ -14,8 +17,15 @@ public class ComplexService {
     @Autowired
     private CompanyBeeStrategy beeStrategy;
 
+    Map<String, CompanyStrategy> strategies = new HashMap<>();
+
+    public void init() {
+        strategies.put("竹间", docking);
+        strategies.put("厂商2", beeStrategy);
+    }
+
     //原业务代码
-    public void complexSomething(){
+    public void complexSomething() {
         System.out.println("做一些复杂的业务处理");
         String readFromDatabase = "竹间";
         switch (readFromDatabase) {
@@ -25,46 +35,33 @@ public class ComplexService {
             case "厂商2":
                 System.out.println("执行对应的处理");
                 break;
-                //后续会根据情况增加很多个不同的case处理
-                default:break;
+            //后续会根据情况增加很多个不同的case处理
+            default:
+                break;
         }
     }
 
     //我尝试用策略模式改造原有代码,没有想到如何以多态来实现,以省略switch的选择
-    public void tryToCompaynStrategy(){
+    public void tryToCompaynStrategy() {
         ServiceTypeA dataA = new ServiceTypeA();
         ServiceTypeB dataB = new ServiceTypeB();
         System.out.println("做一些复杂的业务处理");
         String readFromDatabase = "竹间";
-        switch (readFromDatabase) {
-            case "竹间":
-                docking.docking(dataA);
-                break;
-            case "厂商2":
-                beeStrategy.docking(dataB);
-                break;
-            //后续会根据情况增加很多个不同的case处理
-            default:break;
-        }
+        strategies.getOrDefault(readFromDatabase, new CompanyStrategy() {
+            @Override
+            public void docking(Context context) {
+                throw new IllegalStateException("Unknown strategy: " + readFromDatabase);
+            }
+        }).docking(new Context(dataA, dataB));
     }
 
     //这里使用枚举的方式来替代switch
-    public void tryToEnum(){
+    public void tryToEnum() {
         ServiceTypeA dataA = new ServiceTypeA();
         ServiceTypeB dataB = new ServiceTypeB();
         System.out.println("做一些复杂的业务处理");
         String readFromDatabase = "竹间";
 
-        //此处貌似还是没法避免做到传递正确对应的参数
-        switch (readFromDatabase) {
-            case "竹间":
-                TryEnum.valueOf(readFromDatabase).docking(dataA);
-                break;
-            case "厂商2":
-                TryEnum.valueOf(readFromDatabase).docking(dataB);
-                break;
-            //后续会根据情况增加很多个不同的case处理
-            default:break;
-        }
+        TryEnum.valueOf(readFromDatabase).docking(new Context(dataA, dataB));
     }
 }
--- src/main/java/org/github/geass/service/TryEnum.java
+++ src/main/java/org/github/geass/service/TryEnum.java
@@ -1,33 +1,18 @@
 package org.github.geass.service;
 
-import org.github.geass.dto.ServiceTypeA;
-import org.github.geass.dto.ServiceTypeB;
-
 //用汉字貌似不合适,但为了和数据库读取数据匹配...
-public enum TryEnum implements CompanyStrategy{
+public enum TryEnum implements CompanyStrategy {
 
-    竹间{
+    竹间 {
         @Override
-        public void docking(ServiceTypeA a) {
-            System.out.println("获得对应参数A,执行对应的处理");
-        }
-
-        @Override
-        public void docking(ServiceTypeB a) {
-            //被迫覆盖这个方法
-            throw new RuntimeException("方法调用错误");
+        public void docking(Context context) {
+            System.out.println("获得对应参数A,执行对应的处理" + context.serviceTypeA);
         }
     },
-    厂商2{
+    厂商2 {
         @Override
-        public void docking(ServiceTypeA a) {
-            //被迫覆盖这个方法
-            throw new RuntimeException("方法调用错误");
-        }
-
-        @Override
-        public void docking(ServiceTypeB a) {
-            System.out.println("获得对应参数B,执行对应的处理");
+        public void docking(Context context) {
+            System.out.println("获得对应参数A,执行对应的处理" + context.serviceTypeB);
         }
     }
 }
